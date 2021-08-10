3a3d6fd79e06959a9f71a2e2f7060873c9bbb296
tongoonamujera <tongoonamujera@gmail.com>
2021-08-10 18:54:52 +0200

    Fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,7 @@
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
-#rubocop: disable all
+#rubocop:disable all
 # This file is the source Rails uses to define your schema when running `rails
 # db:schema:load`. When creating a new database, `rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
