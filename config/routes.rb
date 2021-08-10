445edf90b90019a77a4d74937b1cbab660e7b509
tongoonamujera <tongoonamujera@gmail.com>
2021-08-10 16:14:50 +0200

    Add styles for edit method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only:[:create, :new]
+  resources :users
   root 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
